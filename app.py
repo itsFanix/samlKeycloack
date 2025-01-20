1586a072c0270c1053ebdee15d499bdfca42c630
fanny <renatenyatsikor@gmail.com>
2025-01-20 16:38:14 +0100

    add  html file

--- app.py
+++ app.py
@@ -1,24 +1,31 @@
 import os
 
-from flask import Flask, request
+from flask import Flask, request, redirect, session, url_for
 from onelogin.saml2.auth import OneLogin_Saml2_Auth
 from onelogin.saml2.utils import OneLogin_Saml2_Utils
+from urllib.parse import urlparse
 
 
 
 
 
+
+
+#SSO : Signle Sign on
+#SLO : Single Log out
+#ACS : Assertion Consumer Service
 
 
 app = Flask(__name__)
+
+app.config["SAML_PATH"] = os.path.join(os.path.dirname(os.path.abspath(__file__)), "saml")
 
 
 
 def init_saml_auth(req):
     #Initialize with settings.json & advanced_settings.json file
-    auth = OneLogin_saml2_Auth(req)
+    auth = OneLogin_saml2_Auth(req, custom_base_path = app.config["SAML_PATH"])
     return auth
-    #auth
  
 
 
@@ -33,17 +40,73 @@ def preprare_flask_request(request):
     
 
 
+
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
+    if 'samlUserdata' not in session:
+        return redirect(url_for('login'))
+    
+    return f"""
+
+    <h2>Welcome, {session['samlUserdata'].get('username',  'User')}</h2>
+    <a href="{url_for('logout')}">Logout</a>
+    """
+
+
+@app.route('/login')
+def login():
+    req = preprare_flask_request(request)
+    auth = init_saml_auth()
+    return redirect(auth.login())
+
+
+
+
+
+
+@app.route('/acs', methods=['POST'])
+def acs():
     req = preprare_flask_request(request)
     auth = init_saml_auth(req)
-    errors = []
-    error_reason = None
-    not_auth_warn = False
-    success_slo = False
-    attributes = False
-    paint_logout = False
-    return
+    auth.process_response()
+    errors= auth.get_errors()
+
+
+    if not errors:
+        session['samlUserdata'] = auth.get_attributes()
+        session['samlNameId'] = auth.get_nameid()
+        
+        if 'RelayState' in request.form and request.form['RelayState']:
+            return redirect(auth.rediredt_to(request.form['RelayState']))
+        return redirect(url_for('index'))
+    
+    return f"Error: {', '.join(errors)}"
+
+
+
+@app.route('/logout')
+def logout():
+    req = preprare_flask_request(request)
+    auth = init_saml_auth(req)
+    name_id = session.Get('samlNameId')
+    session.clear()
+    return redirect(auth.logout(name_id=name_id))
+
+
+
+@app.route('sls')
+def sls():
+    req = preprare_flask_request(request)
+    auth = init_saml_auth(req)
+    url = auth.process_slo()
+    errors = auth.get_errors()
+
+    if len(errors)==0:
+        return redirect(url or url_for('index'))
+    
+    return f"Error: {','.join(errors)}"
 
 
 @app.route("/attrs/")
